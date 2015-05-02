15e9486b75f58d54f0cece416469d50dff99f7f1
anton44eg <anton.simernya@gmail.com>
2015-05-02 13:57:46 +0530

    add User-Agent

--- canistreamit/canistreamit.py
+++ canistreamit/canistreamit.py
@@ -3,14 +3,22 @@ from functools import partial
 
 def search(movie):
     r = requests.get('http://www.canistream.it/services/search',
-            params={'movieName': movie})
+            params={'movieName': movie}, 
+            headers={
+                'User-Agent': 
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36'
+                    '(KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'})
     return r.json()
 
 def movie(movie_id, media_type):
     r = requests.get('http://www.canistream.it/services/query',
             params={'movieId': movie_id,
                     'attributes': '1',
-                    'mediaType': media_type})
+                    'mediaType': media_type},
+            headers={
+                'User-Agent': 
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36'
+                    '(KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'})
     return r.json()
 
 streaming = partial(movie, media_type='streaming')
