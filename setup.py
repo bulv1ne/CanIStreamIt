f416bf753d1aba39f20af873e9ce728da994f34c
Niels Lemmens <draso.odin@gmail.com>
2015-05-05 11:37:49 +0200

    Version bump to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='CanIStreamIt',
-    version='0.1',
+    version='0.2',
     url='https://github.com/Bulv1ne/CanIStreamIt',
     license='MIT',
     author='Niels Lemmens',
